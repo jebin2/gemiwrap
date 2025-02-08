1c78ee1deb4c4046a092a95ec4586995d61260e1
jebin2 <jebineinstein@gmail.com>
2025-02-08 21:21:01 +0530

    type check added

--- gemiwrap/utils.py
+++ gemiwrap/utils.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import string
 import secrets
 import ffmpeg
+import mimetypes
 
 def load_dotenv_if_exists():
 	if os.path.exists(".env"):
@@ -15,8 +16,12 @@ def generate_random_string(length=10):
     random_string = ''.join(secrets.choice(characters) for _ in range(length))
     return random_string
 
+def is_video_file(file_path):
+    mime_type, _ = mimetypes.guess_type(file_path)
+    return mime_type and mime_type.startswith("video")
+
 def video_duration(file_path):
-	if not os.path.isfile(file_path):
+	if not os.path.isfile(file_path) or not is_video_file(file_path):
 		return 0
 
 	probe = ffmpeg.probe(file_path)
