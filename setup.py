aaafbf15c5f82364334deb50b051c31766661b84
jebin2 <jebineinstein@gmail.com>
2025-03-13 15:35:12 +0530

    name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="gemini_wrapper",
+	name="gemiwrap",
 	packages=find_packages(exclude=["tests*"]),
 	install_requires=[
 		"google-genai",
