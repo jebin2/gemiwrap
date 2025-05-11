dc4657c21cff08537894fe51e12282f629331111
jebin2 <jebineinstein@gmail.com>
2025-05-11 09:02:15 +0530

    reused video if exist

--- gemiwrap/split_video.py
+++ gemiwrap/split_video.py
@@ -1,15 +1,18 @@
 import ffmpeg
 from custom_logger import logger_config
 import os
-from .utils import generate_random_string, video_duration
+from pathlib import Path
+from .utils import video_duration
 
 
 def split(video_path, parts=3):
     logger_config.info(f"Attempting to split video: {video_path} into {parts} parts.")
-    _, ext = os.path.splitext(video_path)
+    path = Path(video_path)
+    name = path.stem
+    ext = path.suffix
     all_files = []
-    temp_dir = os.getenv("TEMP_OUTPUT", "tempOutput")
-    os.makedirs(temp_dir, exist_ok=True)
+    temp_dir = Path(os.getenv("TEMP_OUTPUT", "tempOutput")) / name
+    temp_dir.mkdir(parents=True, exist_ok=True)
 
     duration = video_duration(video_path)
     if duration is None or duration <= 0:
@@ -24,11 +27,14 @@ def split(video_path, parts=3):
     output_acodec = 'copy'
 
     for i in range(parts):
-        output_filename = f'{generate_random_string()}_split_video_{i + 1}{ext}'
-        output_path = os.path.join(temp_dir, output_filename)
+        output_filename = f"{name}_split_video_{i + 1}{ext}"
+        output_path = temp_dir / output_filename
         start_time = i * each_dur
 
-        stream = ffmpeg.input(video_path, ss=start_time)
+        if output_path.exists():
+            continue
+
+        stream = ffmpeg.input(str(video_path), ss=start_time)
         output_args = {
             'acodec': output_acodec,
             'vcodec': output_vcodec,
@@ -39,9 +45,9 @@ def split(video_path, parts=3):
         }
 
         if i < parts - 1:
-            stream = ffmpeg.output(stream, output_path, t=each_dur, **output_args)
+            stream = ffmpeg.output(stream, str(output_path), t=each_dur, **output_args)
         else:
-            stream = ffmpeg.output(stream, output_path, **output_args)
+            stream = ffmpeg.output(stream, str(output_path), **output_args)
 
         logger_config.info(f"Running FFmpeg for part {i+1}: Start={start_time:.2f}" + (f", Duration={each_dur:.2f}" if i < parts-1 else ", Duration=ToEnd"))
         stream.run()
