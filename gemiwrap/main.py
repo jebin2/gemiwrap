74d3a11192276f8509a43fb8f4a85a99240e2b8d
Jebin Einstein <jebineinstein@gmail.com>
2025-10-12 15:29:13 +0530

    block none

--- gemiwrap/main.py
+++ gemiwrap/main.py
@@ -91,7 +91,25 @@ class GeminiWrapper:
 			response_mime_type=self.response_mime_type,
 			response_schema=self.schema,
 			tools=self.tools,
-			thinking_config=self.thinking_config
+			thinking_config=self.thinking_config,
+			safety_settings=[
+				types.SafetySetting(
+					category="HARM_CATEGORY_HARASSMENT",
+					threshold="BLOCK_NONE",  # Block none
+				),
+				types.SafetySetting(
+					category="HARM_CATEGORY_HATE_SPEECH",
+					threshold="BLOCK_NONE",  # Block none
+				),
+				types.SafetySetting(
+					category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+					threshold="BLOCK_NONE",  # Block none
+				),
+				types.SafetySetting(
+					category="HARM_CATEGORY_DANGEROUS_CONTENT",
+					threshold="BLOCK_NONE",  # Block none
+				),
+			],
 		)
 
 	def __send_message_with_timeout(self, user_prompt, config, timeout=300):
